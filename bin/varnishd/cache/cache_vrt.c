d3d5d212f9c2d75e8adebeaef31efc871b558722
Pål Hermunn Johansen <hermunn@varnish-software.com>
2018-02-04 14:31:49 +0100

    Make all purges use ttl_now as their base time
    Since we are calculating all TTLs in VCL from the new ttl_now, we
    also should make hard and soft purges anchor to this time.

--- bin/varnishd/cache/cache_vrt.c
+++ bin/varnishd/cache/cache_vrt.c
@@ -622,7 +622,7 @@ VRT_purge(VRT_CTX, double ttl, double grace, double keep)
 	CHECK_OBJ_NOTNULL(ctx->req, REQ_MAGIC);
 	CHECK_OBJ_NOTNULL(ctx->req->wrk, WORKER_MAGIC);
 	return (HSH_Purge(ctx->req->wrk, ctx->req->objcore->objhead,
-	    ttl, grace, keep));
+	    ctx->ttl_now, ttl, grace, keep));
 }
 
 /*--------------------------------------------------------------------
